8689cb0f1bf085bcd4de3164d5607045d14a1fd4
khwww <khw4420@knu.ac.kr>
2025-05-12 04:00:22 +0900

    remove: App.jsx 삭제

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-500">Hello, World!</h1>
-    </div>
-  );
-}
-
-export default App;
