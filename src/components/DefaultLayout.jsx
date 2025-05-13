3f61272d89ff588a7f0bc70b2c41ae33ea471847
Yongyeon Kim <seyeon.dev@gmail.com>
2025-05-13 10:48:06 +0900

    feat: Header 컴포넌트 추가

--- src/components/DefaultLayout.jsx
+++ src/components/DefaultLayout.jsx
@@ -1,11 +1,17 @@
+import Header from './header/index.jsx';
+
 const DefaultLayout = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div class="bg-democratic text-white">더불어민주당</div>
-      <div class="text-people">국민의힘</div>
-      <div class="text-reform">개혁신당</div>
-      <div className="bg-people text-white font-nanum px-4 py-2 rounded">국민의힘</div>
-    </div>
+    <>
+      <Header />
+      <main className="mw-1280 min-h-screen flex items-center justify-center">
+        <div class="bg-democratic text-white">더불어민주당</div>
+        <div class="text-people">국민의힘</div>
+        <div class="text-reform">개혁신당</div>
+        <div className="bg-people text-white font-nanum px-4 py-2 rounded">국민의힘</div>
+      </main>
+      <footer className="mw-1280"></footer>
+    </>
   );
 };
 
---
+++ src/components/header/index.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header className="mw-1280 md:py-5 py-4 flex justify-between items-center">
+      <h1>
+        <img className="md:w-40 w-24 h-auto" src="/logo.svg" alt="PickMeUp 픽미업" />
+      </h1>
+      {/* theme 버튼 */}
+    </header>
+  );
+}
+
+export default Header;
