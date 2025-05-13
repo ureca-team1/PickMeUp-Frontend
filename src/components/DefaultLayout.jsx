d386c482a2f97fd080b9a836411626eec9314032
Yongyeon Kim <seyeon.dev@gmail.com>
2025-05-13 10:50:01 +0900

    chore: theme 주석 추가

--- src/components/DefaultLayout.jsx
+++ src/components/DefaultLayout.jsx
@@ -5,10 +5,7 @@ const DefaultLayout = () => {
     <>
       <Header />
       <main className="mw-1280 min-h-screen flex items-center justify-center">
-        <div class="bg-democratic text-white">더불어민주당</div>
-        <div class="text-people">국민의힘</div>
-        <div class="text-reform">개혁신당</div>
-        <div className="bg-people text-white font-nanum px-4 py-2 rounded">국민의힘</div>
+        {/* 메인 콘텐츠 */}
       </main>
       <footer className="mw-1280"></footer>
     </>
