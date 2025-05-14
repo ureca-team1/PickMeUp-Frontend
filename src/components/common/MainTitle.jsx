59f2ab3a24c37b9b00fa9356dc42b7a76fbae141
Yongyeon Kim <seyeon.dev@gmail.com>
2025-05-14 15:36:10 +0900

    style: 탭 버튼, swiper 스타일링

--- src/components/common/MainTitle.jsx
+++ src/components/common/MainTitle.jsx
@@ -1,6 +1,10 @@
 function MainTitle({ children, className }) {
   return (
-    <h3 className={`text-[1.75rem] font-extrabold md:text-[3.375rem] ${className}`}>{children}</h3>
+    <h3
+      className={`mb-3 text-[1.75rem] font-extrabold md:mb-[26px] md:text-[3.375rem] ${className}`}
+    >
+      {children}
+    </h3>
   );
 }
 
--- src/components/policySummary/policySummaryComponents/CandidateProfile.jsx
+++ src/components/policySummary/policySummaryComponents/CandidateProfile.jsx
@@ -1,16 +1,12 @@
-import React from 'react';
-
 const CandidateProfile = ({ imgSrc, name, partyColor }) => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center gap-4 md:gap-6">
       <img
         src={imgSrc}
         alt={name}
-        className={`mb-5 h-32 w-32 rounded-full border-[5px] border-black overflow-hidden ${partyColor} `}
+        className={`h-20 w-20 overflow-hidden rounded-full border-[5px] border-black md:h-[100px] md:w-[100px] ${partyColor}`}
       />
-       <p className="text-center text-[24px] font-extrabold leading-[36px] md:text-[32px]">
-        {name}
-      </p>
+      <p className="text-center text-2xl font-extrabold md:text-[2rem]">{name}</p>
     </div>
   );
 };
