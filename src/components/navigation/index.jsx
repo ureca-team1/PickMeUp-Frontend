b4da78a5553fa92fd74ab1de11760c1cec902e0b
Yudongseok <rex010307@gmail.com>
2025-05-14 13:42:17 +0900

    fix:navigation 다크모드 적용

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -56,7 +56,7 @@ function GlobalNavigation() {
   }, []);
 
   return (
-    <nav className="sticky top-0 z-10 flex items-center justify-center gap-1 bg-white py-4 md:gap-30">
+    <nav className="sticky top-0 z-10 flex items-center justify-center gap-1 py-4 md:gap-30">
       {navList.map((item) => (
         <NavLink
           to={`#${item.sectionId}`}
