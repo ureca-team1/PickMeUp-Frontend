28b5082f8063d64ea83945d8588ac51d9a71c0e3
Yongyeon Kim <seyeon.dev@gmail.com>
2025-05-14 17:22:24 +0900

    style: 모의 투표 스타일 수정

--- src/components/vote/voteComponents/RegionSelect.jsx
+++ src/components/vote/voteComponents/RegionSelect.jsx
@@ -1,25 +1,17 @@
-import React from 'react';
-
-// 백엔드 지역 그대로 가져왔습니다.
-const REGIONS = {
-  1: '서울',
-  2: '인천/경기',
-  3: '대전/세종/충청',
-  4: '광주/전라',
-  5: '대구/경북',
-  6: '부산/울산/경남',
-  7: '강원/제주',
-};
+import { REGIONS } from '@/utils/constants.js';
 
 const RegionSelect = ({ onRegionSelect }) => {
   return (
-    <div className="flex w-full items-center justify-between">
-      <label htmlFor="region" className="text-lg whitespace-nowrap text-gray-700 dark:text-white">
+    <div className="mx-auto flex w-full max-w-[700px] items-center justify-between">
+      <label
+        htmlFor="region"
+        className="whitespace-nowrap text-gray-700 md:text-lg dark:text-white"
+      >
         어느 지역에 계신가요?
       </label>
       <select
         id="region"
-        className="w-34 rounded-md border p-2 text-sm sm:w-34 dark:bg-black dark:text-white"
+        className="w-34 rounded-md border p-1.5 text-sm md:p-2 dark:bg-black dark:text-white"
         defaultValue=""
         onChange={(e) => {
           const regionId = Number(e.target.value);
