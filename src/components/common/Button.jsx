46ccfe650a04c3985c7e002dfa7e74a0ec3f6715
Yongyeon Kim <seyeon.dev@gmail.com>
2025-05-15 15:24:20 +0900

    style: 공통 버튼 컴포넌트 스타일 수정

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,10 +1,8 @@
-const Button = ({ children, disabled = false, type = 'button', onClick, className }) => {
+const Button = ({ children, className, ...restProps }) => {
   return (
     <button
-      type={type}
-      disabled={disabled}
-      onClick={onClick}
-      className={`border-primary bg-primary flex h-[42px] w-[101px] items-center justify-center rounded border-2 px-4 py-3 text-sm font-bold whitespace-nowrap text-white hover:cursor-pointer disabled:cursor-not-allowed disabled:opacity-50 md:h-[68px] md:w-[184px] md:px-[46px] md:py-[22px] md:text-2xl dark:bg-gray-500 dark:text-white ${className}`}
+      className={`border-primary bg-primary flex items-center justify-center rounded border-2 px-4 py-3 text-sm font-bold whitespace-nowrap text-white disabled:cursor-not-allowed disabled:opacity-50 md:px-11 md:py-5 md:text-2xl md:font-extrabold dark:bg-gray-500 dark:text-white ${className}`}
+      {...restProps}
     >
       {children}
     </button>
