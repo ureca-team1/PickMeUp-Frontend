5b80cbf3ccc53827c2499351d5c1b67538080594
Yongyeon Kim <seyeon.dev@gmail.com>
2025-05-15 17:21:09 +0900

    style: 실시간 지지율 스타일 수정

--- src/components/approvalRating/RatingTitle.jsx
+++ src/components/approvalRating/RatingTitle.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import MainTitle from '../common/MainTitle';
 import MainParagraph from '../common/MainParagraph';
+import MainTitle from '../common/MainTitle';
 
 const RatingTitle = () => {
   return (
-    <div className="mb-14 text-center">
+    <div className="mb-7 text-center md:mb-14">
       <MainTitle>실시간 지지율</MainTitle>
       <MainParagraph>
         다른 사람들은 누구에게 투표중인지
