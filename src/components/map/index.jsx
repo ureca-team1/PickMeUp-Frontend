f56942e4181c5144cf8ea5d57436ba63bad87199
Yongyeon Kim <seyeon.dev@gmail.com>
2025-05-15 17:36:34 +0900

    fix: 네비게이션 활성화 안되는 이슈 수정 섹션 길이가 길어서 다음 섹션 활성화 안됨

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import MapSearch from './mapComponents/MapSearch';
-import MapList from './mapComponents/MapList';
 import MapInfo from './mapComponents/MapInfo';
+import MapList from './mapComponents/MapList';
+import MapSearch from './mapComponents/MapSearch';
 
 const Map = () => {
   return (
-    <div
-      id="map"
-      className="scroll-margin flex flex-col items-center justify-center px-4 text-center md:px-0"
-    >
+    <div className="mb-24 flex flex-col items-center justify-center px-4 text-center md:mb-52 md:px-0">
       <MapSearch />
       <MapInfo />
       <MapList />
