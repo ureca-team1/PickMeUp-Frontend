54e1319b2e1e46509f85dc7ce445b52ee7fc38b7
khwww <khw4420@knu.ac.kr>
2025-05-15 17:53:12 +0900

    fix: rating img 경로 수정

--- src/apis/ratingApi.js
+++ src/apis/ratingApi.js
@@ -1,13 +1,18 @@
 import axios from '@/utils/axiosInstance';
 
 const CANDIDATE_INFO = {
-  1: { name: '이재명', party: '더불어민주당', partyKey: 'democratic', image: '1.png' },
-  2: { name: '김문수', party: '국민의힘', partyKey: 'people', image: '2.png' },
-  3: { name: '이준석', party: '개혁신당', partyKey: 'reform', image: '3.png' },
-  4: { name: '권영국', party: '민주노동당', partyKey: 'labor', image: '4.png' },
-  5: { name: '구주와', party: '자유통일당', partyKey: 'free', image: '5.png' },
-  6: { name: '황교안', party: '무소속', partyKey: 'independent', image: '6.png' },
-  7: { name: '송진호', party: '무소속', partyKey: 'independent', image: '7.png' },
+  1: {
+    name: '이재명',
+    party: '더불어민주당',
+    partyKey: 'democratic',
+    image: '/candidates/이재명.png',
+  },
+  2: { name: '김문수', party: '국민의힘', partyKey: 'people', image: '/candidates/김문수.png' },
+  3: { name: '이준석', party: '개혁신당', partyKey: 'reform', image: '/candidates/이준석.png' },
+  4: { name: '권영국', party: '민주노동당', partyKey: 'labor', image: '/candidates/권영국.png' },
+  5: { name: '구주와', party: '자유통일당', partyKey: 'free', image: '/candidates/구주와.png' },
+  6: { name: '황교안', party: '무소속', partyKey: 'independent', image: '/candidates/황교안.png' },
+  7: { name: '송진호', party: '무소속', partyKey: 'independent', image: '/candidates/송진호.png' },
   8: { name: '기타', party: '', partyKey: 'primary', image: '' },
 };
 
