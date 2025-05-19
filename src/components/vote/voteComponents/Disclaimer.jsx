2aa885e32b11b805f9759f00a0b92df53040edeb
yshls <shluxnsal01@gmail.com>
2025-05-19 13:22:24 +0900

    fix: 문구 변경

--- src/components/vote/voteComponents/Disclaimer.jsx
+++ src/components/vote/voteComponents/Disclaimer.jsx
@@ -1,7 +1,7 @@
 const Disclaimer = () => {
   return (
     <p className="text-independent mt-6 text-center text-xs leading-[1.8] md:text-base">
-      ※ 본 페이지는 실제 선거와 무관한 모의 체험 콘텐츠입니다. <br />
+      ※ 본 페이지는 실제 선거와 무관한 모의 투표입니다. <br />
       모든 데이터는 사용자 참여 기반의 체험용으로 수집되며, <br />
       공식 지지율이나 선거 결과와는 관련이 없습니다.
     </p>
